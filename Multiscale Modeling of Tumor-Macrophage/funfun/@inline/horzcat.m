edac328bfb6e609cc4e20e5bb8983c8bef1bee03
HaofLin <a550399752@gmail.com>
2024-07-15 18:56:56 +0800

    Delete Multiscale Modeling of Tumor-Macrophage directory

--- Multiscale Modeling of Tumor-Macrophage/funfun/@inline/horzcat.m
+++
@@ -1,7 +0,0 @@
-function horzcat(varargin)
-%HORZCAT Horizontal concatenation of inline objects (disallowed)
-
-%   Steven L. Eddins, August 1995
-%   Copyright 1984-2020 The MathWorks, Inc.
-
-error(message('MATLAB:Inline:horzcat:notAllowed'));
--- Multiscale Modeling of Tumor-Macrophage/funfun/@inline/symvar.m
+++
@@ -1,9 +0,0 @@
-function vars = symvar(s)
-%SYMVAR Determine the symbolic variables for an INLINE.
-%   SYMVAR returns the variables for the INLINE object.
-%
-%   See also ARGNAMES.
-
-%   Copyright 1984-2002 The MathWorks, Inc. 
-
-vars = argnames(s);
